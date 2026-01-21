06d71023b4c956e70a0989132a6c38e68c86c8e7
Lorenzo Caselli <lore.caselli97@gmail.com>
2026-01-21 12:31:09 +0000

    add system test case setup with headless Chrome for GitHub Actions to avoid errors cause by CI running in a headless environment

---
+++ test/application_system_test_case.rb
@@ -0,0 +1,6 @@
+require "test_helper"
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  # 'headless_chrome' is required for GitHub Actions to pass
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+end
